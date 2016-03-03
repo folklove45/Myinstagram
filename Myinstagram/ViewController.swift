09546fecc8cf29489a9c116456ce6a9412cfd321
Patcharin Chunhong <devil_nightmare@hotmail.com>
2016-03-03 12:54:02 +0700

    Initial Commit

---
+++ Myinstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Myinstagram
+//
+//  Created by WebEngineering on 3/3/2559 BE.
+//  Copyright © 2559 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
